11f503656deabdbc08a4580dcd8e06bcd17059ac
Renan Gonçalves <84860067+Dasori157@users.noreply.github.com>
2022-09-03 17:10:44 -0300

    03/09/2022
    17:10

--- Smart Space/Assets/Scripts/BotoesMenu.cs
+++ Smart Space/Assets/Scripts/BotoesMenu.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BotoesMenu : MonoBehaviour
 {
-   
+    public Animator anim;
+    [SerializeField]
+    string cena;
+   public void JogarEsair()
+   {
+        anim.SetTrigger("FadeOut");
+        Invoke("CarregarCena", 1);
+        
+   }
+
+    void CarregarCena()
+    {
+        SceneManager.LoadScene(cena);
+    }
+
+    public void Sair()
+    {
+        anim.SetTrigger("FadeOut");
+        Invoke("Sair2", 1f);
+    }
+
+    void Sair2()
+    {
+        Application.Quit();
+    }
 }
